3c7088935eaa945e0acf07965d963c647a6ed974
jacob <jyablonski9@gmail.com>
2025-01-19 12:31:13 -0800

    Common Modules v0.0.6 (#6)
    ### Description
    Small Logging Fix
    
    ## Added
    - None
    
    ## Updated
    - Base Logger Function to make the `log_file` parameter optional
    - CD Alert Workflow to include the PR Title in the Message
    
    ## Deleted
    - None

--- jyablonski_common_modules/logging/base_logger.py
+++ jyablonski_common_modules/logging/base_logger.py
@@ -1,12 +1,32 @@
 import logging
 
 
-def create_logger(log_file: str, date_format: str = "%Y-%m-%d %I:%M:%S %p"):
+def create_logger(
+    log_file: str | None = None, date_format: str = "%Y-%m-%d %I:%M:%S %p"
+):
+    """
+    Function to create a logger that writes to both STDOUT and a log file if specified.
+
+    Args:
+        log_file (str | None): Optional parameter to specify a Log File for logs to be
+            written out to
+
+        date_format (str): The format of the date in the log message.
+
+    Returns:
+        Python Logger object which writes Logs out as specified
+
+    """
+    handlers = [logging.StreamHandler()]
+
+    if log_file:
+        handlers.append(logging.FileHandler(log_file))
+
     logging.basicConfig(
         level=logging.INFO,
         format="[%(levelname)s] %(asctime)s %(message)s",
         datefmt=date_format,
-        handlers=[logging.FileHandler(log_file), logging.StreamHandler()],
+        handlers=handlers,
     )
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
